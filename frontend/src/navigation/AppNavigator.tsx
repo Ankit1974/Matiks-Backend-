4ee8064127f19355c89c1a5bb84e966f35747e71
Ankit508700 <ankitraj@sftgroups.com>
2026-01-21 23:36:47 +0530

    removed Frontend

--- frontend/src/navigation/AppNavigator.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BottomTabNavigator from './BottomTabNavigator';
-
-const AppNavigator: React.FC = () => {
-    return <BottomTabNavigator />;
-};
-
-export default AppNavigator;
